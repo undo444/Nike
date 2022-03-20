a5a949f270a673010c46b00117558b501b2609ab
Manolache Alexandru <99614597+undo444@users.noreply.github.com>
2022-03-21 01:42:19 +0200

    Add files via upload

---
+++ Register.java
@@ -0,0 +1,68 @@
+package NikeTest;
+
+import Base.BaseTest;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import sun.awt.SunHints;
+
+import java.awt.*;
+import java.security.Key;
+import java.security.KeyStore;
+import java.util.List;
+
+public class Register extends BaseTest {
+
+    @Test
+
+    public void RegisterTest() {
+
+        WebElement SignInButton = Driver.findElement(By.xpath("//span[contains(text(),'Sign In')]"));
+        SignInButton.click();
+
+        WebElement JoinUsClick = Driver.findElement(By.xpath("//a[contains(text(),'Join Us.')]"));
+        JoinUsClick.click();
+
+        WebElement Email = Driver.findElement(By.xpath("//input[@placeholder='Email address']"));
+        Email.sendKeys("undo444@yahoo.com");
+
+        WebElement Password = Driver.findElement(By.xpath("//input[@placeholder='Password']"));
+        Password.sendKeys("Test1234#");
+
+        WebElement FirstName = Driver.findElement(By.xpath("//input[@placeholder='First Name']"));
+        FirstName.sendKeys("Manolache");
+
+        WebElement LastName = Driver.findElement(By.xpath("//input[@placeholder='Last Name']"));
+        LastName.sendKeys("Alexandru");
+
+        WebElement BirthDate = Driver.findElement(By.xpath("//input[@placeholder='Date of Birth']"));
+        BirthDate.sendKeys("13");
+        BirthDate.sendKeys("03");
+        BirthDate.sendKeys("1994");
+        BirthDate.sendKeys(Keys.TAB);
+        BirthDate.sendKeys("1994");
+
+        WebElement Country = Driver.findElement(By.xpath("//select[@name='country']"));
+        Select select = new Select(Country);
+        select.selectByVisibleText("Romania");
+
+        List<WebElement> Gender = Driver.findElements(By.xpath("//ul[@data-componentname='gender']/li"));
+        Gender.get(0).click();
+
+        WebElement News = Driver.findElement(By.xpath("//label[@class='checkbox']"));
+        News.click();
+
+        WebElement JoinUsButton = Driver.findElement(By.xpath("//input[@value='JOIN US']"));
+        JoinUsButton.click();
+
+
+
+
+
+
+
+    }
+}
